2aae9f5d071425fc36dcdf25f4b13ecb886b835a
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-14 20:10:36 +0530

    Added static controls and styling

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
 
 const buildControls = (props) => {
+    const controls = [
+        {label: 'Salad', type: 'salad'},
+        {label: 'Cheese', type: 'cheese'},
+        {label: 'Bacon', type: 'bacon'},
+        {label: 'Meat', type: 'meat'}
+    ];
+
     return (
-        <div>
-            
+        <div className={classes.BuildControls}>
+            {controls.map(control => {
+                return (
+                    <BuildControl key={control.type} label={control.label}/>
+                )
+            })}
         </div>
     );
 }
