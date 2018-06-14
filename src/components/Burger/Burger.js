db3d680e37b268bc911904027326ead9e076abbf
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-14 16:30:12 +0530

    Created static burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.css';
+
+const burger = (props) => {
+	return (
+		<div className={classes.burger}>
+			<BurgerIngredient type="bread-top" />
+			<BurgerIngredient type="cheese" />
+			<BurgerIngredient type="meat" />
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	)
+};
+
+export default burger;
