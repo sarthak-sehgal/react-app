1490947078957f80141155590579faa36afc02d3
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-18 13:12:54 +0530

    Navigation bar complete

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import Logo from '../../assets/logo.png';
 
 const logo = (props) => {
     return (
-        <img className={classes.Logo} src={Logo} alt="My Burger" />
+        <img className={classes.Logo} src={Logo} alt="My Burger" style={{height: props.height}} />
     )
 };
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
+import Navigation from '../NavigationItems/NavigationItems';
 
 const toolbar = () => {
     return (
         <header className={classes.Toolbar}>
-            <div>Menu</div>
-            <Logo />
-            <nav>
-                ...
-            </nav>
+            <Logo height="80%" />
+            <Navigation />
         </header>
     );
 }
